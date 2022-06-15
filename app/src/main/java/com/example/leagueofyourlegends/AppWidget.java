57964f03ec24036d670f70e4ee21b92807907220
letyresina <leticiaresina@gmail.com>
2022-06-15 00:41:16 -0300

    Criação da widget (não testada)

---
+++ app/src/main/java/com/example/leagueofyourlegends/AppWidget.java
@@ -0,0 +1,13 @@
+package com.example.leagueofyourlegends;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class AppWidget extends AppWidgetProvider {
+
+}
